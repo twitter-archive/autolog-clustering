20e5c0566c5c1df62b77b52c3a6cf1fbe7fd8333
Derek Dang <dqdang17@gmail.com>
2018-03-14 18:54:55 -0700

    Added plot

--- naive_bayesian.py
+++ naive_bayesian.py
@@ -2,10 +2,10 @@ import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.naive_bayes import GaussianNB
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 data = pd.read_json("1500.json")
-ma = np.array(data.as_matrix())
+# ma = np.array(data.as_matrix())
 # y_target = ma[:,0]
 # X = ma[:,[1,10]]
 
@@ -19,3 +19,9 @@ y_pred = gnb.fit(X, y_target).predict(X)
 y_prob = gnb.predict_proba(X)
 
 print("Percentage of mislabeled points out of a total %d points : %d" % ((X.shape[0],(y_target != y_pred).sum()/X.shape[0]*100)) + "%")
+
+# print(y_prob)
+
+plt.scatter(y_prob, y_prob)
+
+plt.show()
