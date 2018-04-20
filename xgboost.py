697c618c04fbaea5b1df1c0e310adb4e75cb2fe8
Derek Dang <dqdang17@gmail.com>
2018-04-19 17:38:22 -0700

    fix bugs with score

--- xgboost.py
+++ xgboost.py
@@ -1,6 +1,8 @@
 # ------------------------------
 # IMPORT LIBRARIES
 # ------------------------------
+import warnings
+warnings.filterwarnings(action="ignore", module="scipy", message="^internal gelsd")
 
 import pandas as pd
 # import matplotlib.pyplot as plt
@@ -8,7 +10,7 @@ from sklearn.decomposition import PCA
 from sklearn.ensemble import GradientBoostingClassifier
 
 import xgboost as xgb
-# from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
@@ -44,6 +46,7 @@ websites = pd.read_csv("malicious.csv")
 
 # # Remove any rows without packets.
 websites = websites[websites["APP_PACKETS"] > 0]
+
 # # Remove any rows with missing values.
 websites = websites.dropna(axis=0)
 
@@ -54,11 +57,13 @@ websites = websites.dropna(axis=0)
 # Initialize the model with 2 parameters -- number of clusters and random state.
 xgboost = GradientBoostingClassifier()
 # Get only the numeric columns from websites.
+
 good_columns = websites._get_numeric_data()
+good_columns = good_columns.drop(["Type"], axis=1)
 # Fit the model using the good columns.
-xgboost.fit(good_columns, websites["APP_PACKETS"])
+xgboost.fit(good_columns, websites["Type"])
 # Get the cluster assignments.
-print(xgboost.score(good_columns, websites["APP_PACKETS"]))
+print("This is the score: " + str(xgboost.score(good_columns, websites["Type"])))
 
 
 # # ------------------------------
@@ -79,54 +84,51 @@ print(xgboost.score(good_columns, websites["APP_PACKETS"]))
 # # Show the plot.
 # plt.show()
 
-# # ------------------------------
-# # FIGURING OUT WHAT TO PREDICT
-# # ------------------------------
+# ------------------------------
+# FIGURING OUT WHAT TO PREDICT
+# ------------------------------
 
-# # # Correlation between average_rating and each of the other columns.
-# # # This will show us which other columns might predict average_rating the best.
-# # print(websites.corr()["average_rating"])
+# # Correlation between app_packets and each of the other columns.
+# # This will show us which other columns might predict app_packets the best.
+# print(websites.corr()["APP_PACKETS"])
 
-# # Remove ratings that might be related to average ratings.
-# # Get all the columns from the dataframe.
-# columns = websites.columns.tolist()
-# # Filter the columns to remove ones we don't want.
-# columns = [c for c in columns if c not in ["bayes_average_rating", "average_rating", "type", "name"]]
+# Remove ratings that might be related to average ratings.
+# Get all the columns from the dataframe.
+columns = websites.columns.tolist()
+# Filter the columns to remove ones we don't want.
+columns = [c for c in columns if c in ["SOURCE_APP_PACKETS", "REMOTE_APP_PACKETS", "APP_PACKETS", "SOURCE_APP_BYTES", "REMOTE_APP_BYTES", "APP_BYTES"]]
 
-# # Store the variable we'll be predicting on.
-# target = "average_rating"
-# # print(columns)
+# Store the variable we'll be predicting on.
+target = "APP_PACKETS"
 
-# # ------------------------------
-# # SPLITTING INTO TRAIN AND TEST SETS
-# # ------------------------------
+# ------------------------------
+# SPLITTING INTO TRAIN AND TEST SETS
+# ------------------------------
 
-# # Import a convenience function to split the sets.
-# # Generate the training set.  Set random_state to be able to replicate results.
-# # train = websites.sample(frac=0.8, random_state=1)
-# # Select anything not in the training set and put it in the testing set.
-# # test = websites.loc[~websites.index.isin(train.index)]
-# # Print the shapes of both sets.
-# # print(train.shape)
-# # print(test.shape)
+# Generate the training set.  Set random_state to be able to replicate results.
+train = websites.sample(frac=0.8, random_state=1)
+# Select anything not in the training set and put it in the testing set.
+test = websites.loc[~websites.index.isin(train.index)]
+# Print the shapes of both sets.
+# print(train.shape)
+# print(test.shape)
 
-# # ------------------------------
-# # FIT A LINEAR REGRESSION
-# # ------------------------------
+# ------------------------------
+# FIT A LINEAR REGRESSION
+# ------------------------------
 
-# # Import the linear regression model.
-# # Initialize the model class.
-# # model = LinearRegression()
-# # Fit the model to the training data.
-# # model.fit(train[columns], train[target])
+# Initialize the model class.
+model = LinearRegression()
+# Fit the model to the training data.
+model.fit(train[columns], train[target])
 
-# # ------------------------------
-# # PREDICTING ERROR
-# # ------------------------------
+# ------------------------------
+# PREDICTING ERROR
+# ------------------------------
 
-# # Import the scikit-learn function to compute error.
-# # Generate our predictions for the test set.
-# # predictions = model.predict(test[columns])
+# Import the scikit-learn function to compute error.
+# Generate our predictions for the test set.
+predictions = model.predict(test[columns])
 
-# # Compute error between our test predictions and the actual values.
-# # print(mean_squared_error(predictions, test[target]))
+# Compute error between our test predictions and the actual values.
+print("This is the mean squared error: " + str(mean_squared_error(predictions, test[target])))
